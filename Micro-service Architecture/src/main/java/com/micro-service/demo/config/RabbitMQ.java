f373aa495902971e62b019b3313abd711b2b2ca2
Eddie Hsu <aoe102198@gmail.com>
2024-07-06 23:44:45 +0800

    Configure and setup the RabbitMQ

---
+++ Micro-service Architecture/src/main/java/com/micro-service/demo/config/RabbitMQ.java
@@ -0,0 +1,12 @@
+// RabbitMQConfig.java
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public Queue orderQueue() {
+        return new Queue("orderQueue");
+    }
+}
