c491a6c4bf582e890eb42d0059fac971126613f9
Eddie Hsu <aoe102198@gmail.com>
2024-07-08 23:43:42 +0800

    Add Spring Cloud Gateway into java project
    - Update pom.xml dependency
    - Configure Spring Cloud Gateway properties
    - Update SpringBootApplication and add @EnableDiscoveryClient

--- Micro-service Architecture/src/main/java/com/micro-service/demo/Application.java
+++ Micro-service Architecture/src/main/java/com/micro-service/demo/Application.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
